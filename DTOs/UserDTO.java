3e467b60a4bafe9c2334dcca5c2f53cd138efecc
ThiagoCCR <thiago.ccr96@gmail.com>
2023-04-10 18:42:12 -0300

    feat: UserDTO & User model

---
+++ DTOs/UserDTO.java
@@ -0,0 +1,17 @@
+package com.api.twiteroo.DTOs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UserDTO(
+
+        @NotBlank String username,
+
+        @NotBlank  @NotNull String avatar
+        
+) {
+
+}
+
+
+// @Pattern(regexp = "[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%.\+#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%\+.#?&//=]*)")
