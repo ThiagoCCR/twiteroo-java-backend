06f66cbff965b0b4c305eb9c5e03a87f3eda4753
ThiagoCCR <thiago.ccr96@gmail.com>
2023-04-11 10:09:18 -0300

    feat: tweet Controller && Service

--- controllers/TweetController.java
+++ controllers/TweetController.java
@@ -3,6 +3,7 @@ package com.api.twiteroo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,13 +31,13 @@ public class TweetController {
         return "OK";
     }
 
-    // @GetMapping
-    // public List<Tweet> findAll(@RequestParam(defaultValue = "0") int page) {
-    //     return service.findAll(page);
-    // }
+    @GetMapping
+    public Page<Tweet> findAll(@RequestParam(defaultValue = "0") int page) {
+        return service.findAll(page);
+    }
 
-    // @GetMapping("/{username}")
-    // public List<Tweet> find(@PathVariable String username) {
-    //     return service.findByUser(username);
-    // }
+    @GetMapping("/{username}")
+    public List<Tweet> find(@PathVariable String username) {
+        return service.findByUser(username);
+    }
 }
--- services/TweetService.java
+++ services/TweetService.java
@@ -3,6 +3,9 @@ package com.api.twiteroo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.api.twiteroo.DTOs.PostTweetDTO;
@@ -10,6 +13,8 @@ import com.api.twiteroo.models.Tweet;
 import com.api.twiteroo.models.User;
 import com.api.twiteroo.repositories.TweetRepository;
 import com.api.twiteroo.repositories.UserRepository;
+
+import org.springframework.data.domain.Sort;
 
 @Service
 public class TweetService {
@@ -26,6 +31,18 @@ public class TweetService {
 
         return tweetRepository.save(new Tweet(data, user.getAvatar()));
 
+    }
+
+    public Page<Tweet> findAll(int page) {
+
+        PageRequest pageRequest = PageRequest.of(page, 5, Sort.Direction.DESC, "id");
+
+        return tweetRepository.findAll(pageRequest);
+
+    }
+
+    public List<Tweet> findByUser(String username) {
+        return tweetRepository.findByUsername(username);
     }
 
 }
